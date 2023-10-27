5d11201314291033e4383eac5534fc116bf7df4e
expandsource <expandsource@gmail.com>
2023-10-27 12:45:13 +0900

    ItemFormDto, 뷰 유효성 검증

--- src/main/java/com/busanit/jpashop/controller/ItemController.java
+++ src/main/java/com/busanit/jpashop/controller/ItemController.java
@@ -1,12 +1,25 @@
 package com.busanit.jpashop.controller;
 
+import com.busanit.jpashop.dto.ItemDto;
 import com.busanit.jpashop.dto.ItemFormDto;
+import com.busanit.jpashop.service.ItemService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
 
     @GetMapping("/admin/item/new")
     public String itemForm(Model model) {
@@ -14,4 +27,16 @@ public class ItemController {
         return "/item/itemForm";
     }
 
+    @PostMapping("/admin/item/new")
+    public String itemNew(@Valid ItemFormDto itemFormDto,
+                          BindingResult bindingResult,                          @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList ) {
+        // 유효성 검증 : 통과하지 못한 경우 폼으로
+        if (bindingResult.hasErrors()) {
+            return "/item/itemForm";
+        }
+        // 아이템 DB에 저장
+        itemService.saveItem(itemFormDto, itemImgFileList);
+        // 리다이렉트
+        return "redirect:/";
+    }
 }
---
+++ src/main/java/com/busanit/jpashop/service/ItemService.java
@@ -0,0 +1,12 @@
+package com.busanit.jpashop.service;
+
+import com.busanit.jpashop.dto.ItemFormDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public class ItemService {
+    public void saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) {
+
+    }
+}
