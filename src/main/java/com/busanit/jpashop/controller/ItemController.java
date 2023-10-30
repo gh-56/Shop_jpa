5f220749c1b363e9c4338b242dbd93bbb7868b08
expandsource <expandsource@gmail.com>
2023-10-30 14:25:30 +0900

    대표 상품이미지 있을 경우만 등록 가능하게 컨트롤러, 뷰 수정

--- src/main/java/com/busanit/jpashop/controller/ItemController.java
+++ src/main/java/com/busanit/jpashop/controller/ItemController.java
@@ -31,11 +31,18 @@ public class ItemController {
 
     @PostMapping("/admin/item/new")
     public String itemNew(@Valid ItemFormDto itemFormDto,
-                          BindingResult bindingResult,                          @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList ) {
+                          BindingResult bindingResult,                          @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList, Model model ) {
         // 유효성 검증 : 통과하지 못한 경우 폼으로
         if (bindingResult.hasErrors()) {
             return "/item/itemForm";
         }
+
+        // 상품 이미지 파일 1개를 필수적으로 넣는 유효성 검증 추가
+        if (itemImgFileList.get(0).isEmpty()) {
+            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값입니다.");
+            return "/item/itemForm";
+        }
+
         // 아이템 DB에 저장
         itemService.saveItem(itemFormDto, itemImgFileList);
         // 리다이렉트
@@ -63,6 +70,10 @@ public class ItemController {
     @PostMapping("/admin/item/{itemId}")
     public String itemUpdate(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList, Model model ) {
 
+        // 유효성 검증 : 통과하지 못한 경우 폼으로
+        if (bindingResult.hasErrors()) {
+            return "/item/itemForm";
+        }
 
         // 서비스 계층에 수정 비즈니스 로직 위임
         itemService.updateItem(itemFormDto, itemImgFileList);
