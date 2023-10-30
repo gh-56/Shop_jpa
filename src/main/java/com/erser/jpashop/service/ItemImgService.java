9c63c8856e87662511198c7eadcbbe4eae0b00de
LGH <dinggul56@gmail.com>
2023-10-30 13:54:42 +0900

    feat: 상품 이미지 파일 서비스 계층 추가 및 수정(POST) 요청 변경 감지 기능 활용

--- src/main/java/com/erser/jpashop/service/ItemImgService.java
+++ src/main/java/com/erser/jpashop/service/ItemImgService.java
@@ -2,6 +2,7 @@ package com.erser.jpashop.service;
 
 import com.erser.jpashop.entity.ItemImg;
 import com.erser.jpashop.repository.ItemImgRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -33,5 +34,21 @@ public class ItemImgService {
         // 상품 이미지 정보 저장
         itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
+    }
+
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) {
+        // 예외처리 :  파일 입력 태그가 수정이 된 경우만 로직을 수행
+        if(!itemImgFile.isEmpty()){
+            ItemImg itemImg = itemImgRepository.findById(itemImgId).orElseThrow(EntityNotFoundException::new);
+
+            String oriImgName = itemImgFile.getOriginalFilename();
+            // 파일 업로드 - 리턴 UUID 파일이름
+            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile);
+            // 경로
+            String imgUrl = "/images/item/" + imgName;
+            itemImg.updateItemImg(oriImgName, imgName, imgUrl);
+            // 변경 감지 이미지 변경이 발생
+        }
+
     }
 }
