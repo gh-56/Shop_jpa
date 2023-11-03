f6f77f90b3ce592375501a9febb9eca29a7fbd55
LGH <dinggul56@gmail.com>
2023-11-03 15:53:13 +0900

    장바구니 컨트롤러 예외 처리 및 유효성 검증 추가

--- src/main/java/com/busanit/jpashop/controller/CartController.java
+++ src/main/java/com/busanit/jpashop/controller/CartController.java
@@ -2,15 +2,19 @@ package com.busanit.jpashop.controller;
 
 import com.busanit.jpashop.dto.CartItemDto;
 import com.busanit.jpashop.service.CartService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,13 +24,28 @@ public class CartController {
     // CREATE : 장바구니 담기
     @PostMapping(value = "/cart")
     @ResponseBody
-    public ResponseEntity addCart(@RequestBody CartItemDto cartItemDto, Principal principal){
-
-        // 예외처리
-
+    public ResponseEntity addCart(@RequestBody @Valid CartItemDto cartItemDto, Principal principal
+    , BindingResult bindingResult){
+        // 예외처리 : 유효성 검증 에러 발생시 에러 메시지와 함께 400 리
+        if (bindingResult.hasErrors()){
+            StringBuilder message = new StringBuilder();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                message.append(fieldError.getDefaultMessage());
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message.toString());
+        }
         // 서비스 위임
         String email = principal.getName();
-        Long cartItemId = cartService.addCart(cartItemDto, email);
+        Long cartItemId;
+        try {
+
+            cartItemId = cartService.addCart(cartItemDto, email);
+        }catch (Exception e){
+            // 서비스 계층 예외 발생시 => 400
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
 
         return ResponseEntity.status(HttpStatus.OK).body(cartItemId);
     }
--- src/main/java/com/busanit/jpashop/dto/CartItemDto.java
+++ src/main/java/com/busanit/jpashop/dto/CartItemDto.java
@@ -1,5 +1,7 @@
 package com.busanit.jpashop.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +9,8 @@ import lombok.Setter;
 @Getter @Setter
 public class CartItemDto {
 
+    @NotNull(message = "상품 아이디는 필수 입력 값입니다.")
     private Long itemId;    // 상품 아이템
+    @Min(value = 1, message = "최소 수량은 1입니다.")
     private int count;      // 수량
 }
